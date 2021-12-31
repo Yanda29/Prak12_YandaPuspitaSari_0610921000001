7c886c714b47f99e0e771238897d42f9fec9f9f3
Yanda29 <91973633+Yanda29@users.noreply.github.com>
2021-12-31 20:14:45 +0700

    Add files via upload

---
+++ yanda_prak12-1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 31 13:57:02 2021
+
+@author: YandaPuspitaSari
+"""
+print("Linear Search")
+
+jumlahAngka = int(input("Masukkan jumlah angka pada list: "))
+listAngka = [int(input(f"Masukkan angka ke-{x+1}: ")) for x in range(jumlahAngka)]
+
+print(f"\nList Angka -> {listAngka}")
+cariAngka = int(input("Masukkan angka yang dicari: "))
+for x in listAngka:
+    if x == cariAngka:
+        print(f"Hasil Linear Search -> Ditemukan pada index ke-{listAngka.index(x)}")
+        exit(0)
+
+print("Hasil Linear Search -> Tidak Ditemukan")
